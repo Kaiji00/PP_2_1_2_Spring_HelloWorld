285cc32057daeeb2c4fa1b81d80e34a17cd30bfa
root <stgorbissi@gmail.com>
2022-12-15 10:30:27 +0300

    firstSolution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getValue());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getValue());
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat.equals(cat1));
     }
 }
